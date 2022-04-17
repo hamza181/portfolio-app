7dd1772666cff252b6921128aa0e4fefc48af384
Ameer Hamza <ameer.hamza1811@gmail.com>
2022-04-17 20:48:08 +0500

    add middleware in /about

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
 
+//  Middleware
+
+const middleware = (req, res, next) => {
+    console.log('hello middleware');
+    next();
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get("/about", (req, res) => {
+app.get("/about", middleware, (req, res) => {
   res.send("Hello about!");
 });
 app.get("/contact", (req, res) => {
   res.send("Hello contafct!");
+});
+app.get("/signin", (req, res) => {
+  res.send("Hello signin!");
+});
+app.get("/signup", (req, res) => {
+  res.send("Hello signup!");
 });
 
 app.listen(5000, () => {
