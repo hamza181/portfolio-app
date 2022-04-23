da4b3476483ba9324a19a066257bd5614b7f60fd
Ameer Hamza <ameer.hamza1811@gmail.com>
2022-04-24 00:40:52 +0500

    create error page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import Error from "./components/Error";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="contact" element={<Contact />} />
         <Route path="signup" element={<Signup />} />
         <Route path="login" element={<Login />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </>
   );
